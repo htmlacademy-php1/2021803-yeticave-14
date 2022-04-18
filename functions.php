4f16297fbe7478d2dd83a7132638197630e392de
Sergey <zaycev.sergey@mail.ru>
2022-04-18 19:13:34 +0300

    Внёс правки в функцию по работе с датой

--- functions.php
+++ functions.php
@@ -3,18 +3,14 @@ function price_format($price)
 {
     return number_format(ceil($price), 0, '', ' ') . ' ₽';
 }
-function remaining_time(string $closetime, string $nowtime): array
+function remaining_time(string $closeTime, string $nowTime): array
 {
-    $dt_diff = strtotime($closetime) - strtotime($nowtime);
-    if (is_date_valid($closetime) == false || $dt_diff < 0) {
-        $time = ['hours' => 0, 'minutes' => 0];
-        return $time;
+    $dt_diff = strtotime($closeTime) - strtotime($nowTime);
+    if (!is_date_valid($closeTime) || $dt_diff < 0) {
+        return [0, 0];
     }
     $hours = floor($dt_diff / 3600);
     $minutes = floor($dt_diff % 3600 / 60);
-    $time = [
-        'hours' => $hours,
-        'minutes' => $minutes
-    ];
+    $time = [$hours, $minutes];
     return $time;
 }
--- templates/main.php
+++ templates/main.php
@@ -27,12 +27,12 @@
                             <span class="lot__amount">Стартовая цена</span>
                             <span class="lot__cost"><?= price_format(htmlspecialchars($value['price'])); ?></span>
                         </div>
+                        <?php $time = remaining_time(htmlspecialchars($value['date_expiration']), date('H:i')); ?>
                         <div class="lot__timer timer
-                            <?php $time = remaining_time(htmlspecialchars($value['date_expiration']), date('H:i')); ?>
-                            <?php if ($time['hours'] < 1) {
+                            <?php if ($time[0] < 1) {
                                 echo 'timer--finishing';
                             } ?>">
-                            <?= str_pad($time['hours'], 2, "0", STR_PAD_LEFT) ?>:<?= str_pad($time['minutes'], 2, "0", STR_PAD_LEFT) ?>
+                            <?= str_pad($time[0], 2, "0", STR_PAD_LEFT) ?>:<?= str_pad($time[1], 2, "0", STR_PAD_LEFT) ?>
                         </div>
                     </div>
                 </div>
