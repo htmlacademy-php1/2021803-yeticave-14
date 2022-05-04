9ffe2583e5a06bc3cf1197bbc9eac71a85c044c8
Sergey <zaycev.sergey@mail.ru>
2022-05-04 16:50:02 +0300

    Добавлены две функции по работе с БД

--- functions.php
+++ functions.php
@@ -13,3 +13,30 @@ function remaining_time(string $closeTime, string $nowTime): array
     $minutes = floor($dt_diff % 3600 / 60);
     return [$hours, $minutes];
 }
+//Получение списка новых лотов
+function get_lots(mysqli $link): array
+{
+    $sql = "SELECT l.*,c.name as cat_name 
+FROM lot l
+JOIN category c ON c.id=l.category_id
+WHERE l.finished_date > CURDATE()
+ORDER by l.created_date DESC LIMIT 6";
+    $result = mysqli_query($link, $sql);
+    if ($result) {
+        return mysqli_fetch_all($result, MYSQLI_ASSOC);
+    } else {
+        print("Error MySQL: " . mysqli_error($link));
+    }
+}
+
+//Получение списка категорий
+function get_cat(mysqli $link): array
+{
+    $sql = "SELECT * FROM category";
+    $result = mysqli_query($link, $sql);
+    if ($result) {
+        return mysqli_fetch_all($result, MYSQLI_ASSOC);
+    } else {
+        print("Error MySQL: " . mysqli_error($link));
+    }
+}
