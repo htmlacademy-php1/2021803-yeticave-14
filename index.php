ecbee8713828f3a6adc6db29d9ae767f3154c463
Sergey <zaycev.sergey@mail.ru>
2022-05-04 16:51:10 +0300

    Работа с MySQL

--- index.php
+++ index.php
@@ -1,19 +1,26 @@
 <?php
 
 /**
- * @var array $products
+ * @var array $lots
  * @var array $categories
  * @var string $title
  * @var string $user_name
  * @var bool $is_auth
+ * @var mysqli $link
  */
-require_once('helpers.php');
-require_once('data.php');
-require_once('functions.php');
-$page_content = include_template('main.php', ['products' => $products, 'categories' => $categories]);
+require_once 'helpers.php';
+require_once 'data.php';
+require_once 'functions.php';
+require_once 'init.php';
+
+$lots = get_lots($link);
+$cat = get_cat($link);
+
+
+$page_content = include_template('main.php', ['lots' => $lots, 'categories' => $cat]);
 $layout_content = include_template('layout.php', [
     'content' => $page_content,
-    'categories' => $categories,
+    'categories' => $cat,
     'title' => $title,
     'user_name' => $user_name,
     'is_auth' => $is_auth
