2bf81ec8b4b83137e35fd0c9a46368b8b73ed530
Sergey <zaycev.sergey@mail.ru>
2022-05-06 14:55:24 +0300

    Исправил подключение к БД

--- init.php
+++ init.php
@@ -1,6 +1,5 @@
 <?php
-$db = require_once 'db.php';
-$link = mysqli_connect($db['host'], $db['user'], $db['password'], $db['database']);
+$link = mysqli_connect("127.0.0.1", "root", "", "yeticave");
 mysqli_set_charset($link, "utf8");
 if (!$link) {
     print("Error MySQL: " . mysqli_connect_error());
