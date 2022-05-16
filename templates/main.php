42e5b3e8372bb5627b957b9eec4b3545c984b7b8
Sergey <zaycev.sergey@mail.ru>
2022-05-16 10:40:50 +0300

    Добавил переход на страницу лота

--- templates/main.php
+++ templates/main.php
@@ -31,7 +31,7 @@
                 </div>
                 <div class="lot__info">
                     <span class="lot__category"><?= htmlspecialchars($value['cat_name']); ?> </span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= htmlspecialchars($value['name']); ?></a></h3>
+                    <h3 class="lot__title"><a class="text-link" href="/lot.php?id=<?= htmlspecialchars($value['id']); ?>"><?= htmlspecialchars($value['name']); ?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
