c961796b2eefa7e08e7cf5a1e2c9b681bc79af9c
Sergey <zaycev.sergey@mail.ru>
2022-05-27 13:06:56 +0300

    сценарий добавления лота и вёрстка

--- add.php
+++ add.php
@@ -6,7 +6,7 @@
  * @var string $user_name
  * @var bool $is_auth
  * @var mysqli $link
- * @var string $lots_category
+ * @var array $errors
  */
 
 require_once 'helpers.php';
@@ -14,57 +14,34 @@ require_once 'data.php';
 require_once 'functions.php';
 require_once 'init.php';
 
+$categories = get_categories($link);
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $categories = get_categories($link);
     $lot = filter_input_array(INPUT_POST, [
-        'name' => FILTER_DEFAULT, 'category_id' => FILTER_DEFAULT,
-        'description' => FILTER_DEFAULT, 'initial_price' => FILTER_DEFAULT, 'bid_step' => FILTER_DEFAULT, 'finished_date' => FILTER_DEFAULT
+        'name' => FILTER_DEFAULT, 'category_id' => FILTER_DEFAULT, 'finished_date' => FILTER_DEFAULT,
+        'description' => FILTER_DEFAULT, 'img_url' => FILTER_DEFAULT, 'initial_price' => FILTER_DEFAULT,  'bid_step' => FILTER_DEFAULT
     ], true);
 
-    $required_fields = ['name', 'category_id', 'description', 'lot-img', 'initial_price', 'bid_step', 'finished_date'];
-    $errors = [];
-
-    foreach ($required_fields as $field) {
-        if (empty($_POST[$field])) {
-            $errors[$field] = 'Поле не заполнено';
+    $categories_id = array_column($categories, 'id');
+    $errors = validate_form_add_lot($lot, $categories_id, $_FILES);
+    $errors = array_filter($errors);
+    if (empty($errors)) {
+        $result = add_lot($link, $lot, $_FILES);
+        if ($result) {
+            $lot_id = mysqli_insert_id($link);
+            header("Location: lot.php?id=" . $lot_id);
+        } else {
+            header("Location:/404.php");
         }
     }
 
-    $rules = [
-        'name' => function ($name) {
-            return validateFilled($name);
-        },
-        'category_id' => function ($id) use ($categories) {
-            return validateCategory($id, $categories);
-        },
-        'description' => function ($description) {
-            return validateLength($description, 20, 200);
-        },
-        'initial_price' => function ($initial_price) {
-            return validatePrice($initial_price);
-        },
-        'bid_step' => function ($bid_step) {
-            return validateBidStep($bid_step);
-        },
-        'finished_date' => function ($finished_date) {
-            return is_date_valid($finished_date);
-        }
-    ];
+    $page_content = include_template('add.php', ['categories' => $categories, 'errors' => $errors]);
+    $layout_content = include_template('layout.php', [
+        'content' => $page_content,
+        'categories' => $categories,
+        'title' => 'Добавление лота',
+        'user_name' => $user_name,
+        'is_auth' => $is_auth
+    ]);
+    print($layout_content);
 }
-foreach ($_POST as $key => $value) {
-    if (isset($rules[$key])) {
-        $rule = $rules[$key];
-        $errors[$key] = $rule($value);
-    }
-}
-
-
-$page_content = include_template('add.php', ['categories' => $categories]);
-$layout_content = include_template('layout.php', [
-    'content' => $page_content,
-    'categories' => $categories,
-    'title' => 'Добавление лота',
-    'user_name' => $user_name,
-    'is_auth' => $is_auth
-]);
-print($layout_content);
